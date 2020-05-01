6b9af80af3005f359b40aa326c96c3e06d4c978b
Arushi Jain <58386588+Arushi0302@users.noreply.github.com>
2020-05-02 04:18:28 +0530

    Update create.js

--- create.js
+++ create.js
@@ -12,7 +12,7 @@ function subimit(){
     var db = firebase.firestore();
     var phoneno = /^\d{10}$/;
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(maintext1.value.match(" ")){
+    if(maintext1.value.match("")){
         alert("Please enter valid title");
     }
   
@@ -30,7 +30,35 @@ function subimit(){
             window.history.back();
             
         });
-        sb.disabled = true;
+       
         
     }
 }
+function newElement() {
+    var li = document.createElement("li");
+    var li1 = document.createElement("li1");
+    var li2 = document.createElement("li2");
+    var li3 = document.createElement("li3");
+
+    var inputValue = document.getElementById("maintext1").value;
+    var inputValue1 = document.getElementById("maintext2").value;
+    var inputValue2 = document.getElementById("maintext5").value;
+    var inputValue3 = document.getElementById("maintext6").value;
+
+    var t = document.createTextNode(inputValue);
+    var t1 = document.createTextNode(inputValue1);
+    var t2 = document.createTextNode(inputValue2);
+    var t3 = document.createTextNode(inputValue3);
+
+    li.appendChild(t);
+    li1.appendChild(t1);
+    li2.appendChild(t2);
+    li3.appendChild(t3);
+
+    document.getElementById("c1").appendChild(li);
+    document.getElementById("c2").appendChild(li1);
+    document.getElementById("c3").appendChild(li2);
+    document.getElementById("c4").appendChild(li3);
+    
+   }
+
